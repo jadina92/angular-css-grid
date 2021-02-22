85babd26351281a88887d07f1b35af312568ebb9
jadina92 <dina.iraki@me.com>
2021-02-22 15:15:39 +0100

    angular/css-grid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{ path:'home',component:MainComponent},
+{ path:'privacy-policy',component:PrivacyPolicyComponent},
+{ path:'',pathMatch:'full',redirectTo:'home'},
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
